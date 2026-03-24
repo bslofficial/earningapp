baeed0b58334c232ccd749fbb19cdc553ee5cc5d
Earning Goal <bslgaimerofficial@gmail.com>
2026-03-24 13:30:08 +0600

    Update spin.js

--- spin.js
+++ spin.js
@@ -1,14 +1,13 @@
-const rewards = [1,2,5,0,10];
-
-window.spin = async ()=>{
-    const reward = rewards[Math.floor(Math.random()*rewards.length)];
-
-    const user = auth.currentUser;
-    const snap = await get(ref(db,'users/'+user.uid));
-
-    await update(ref(db,'users/'+user.uid), {
-        balance: snap.val().balance + reward
-    });
-
-    alert("You won ৳" + reward);
-};
+export function spinWheel(){
+ const user = auth.currentUser;
+ if(!user) return;
+ const reward = Math.floor(Math.random()*10)+1; // 1–10 ৳
+ get(ref(db,'users/'+user.uid)).then(snap=>{
+  const balance = snap.val().balance||0;
+  update(ref(db,'users/'+user.uid),{
+   balance: balance+reward
+  });
+  alert("🎉 Lucky Spin Reward: ৳"+reward);
+ });
+}
+window.spinWheel = spinWheel;
